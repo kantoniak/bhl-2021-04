cbe4cae4deeea3499e57e0364c0fa15908eebba6
Krzysztof Antoniak <krzysztof@antoniak.in>
2021-04-09 22:40:42 +0200

    add digits

--- display/segment.py
+++ display/segment.py
@@ -7,6 +7,18 @@ pins = [
     20, 21, 26, 13, 6, 16, 19 # rightmost digit
 ]
 
+# Maps digit to list of segments, where 0=A, 1=B etc.
+digits = [ 
+    [1, 2],
+    [0, 1, 3, 4, 6],
+    [0, 1, 2, 3, 6],
+    [1, 2, 5, 6],
+    [0, 2, 3, 5, 6],
+    [0, 2, 3, 4, 5, 6],
+    [0, 1, 2],
+    [0, 1, 2, 3, 4, 5, 6]
+]
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
@@ -15,9 +27,8 @@ GPIO.output(pins, GPIO.HIGH)
 
 while (True):
     for i in range(len(pins)):
-        GPIO.output(pins[i], GPIO.LOW)
+        GPIO.output(digits[i], GPIO.LOW)
         time.sleep(0.2)
-        GPIO.output(pins[i], GPIO.HIGH)
-    break
+        GPIO.output(digits[i], GPIO.HIGH)
 
 GPIO.output(pins, GPIO.LOW)
