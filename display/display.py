4c686de1502af2c1e7cf5318abe2437ce07e2b73
Krzysztof Antoniak <krzysztof@antoniak.in>
2021-04-09 19:35:52 +0200

    Update

--- display/display.py
+++ display/display.py
@@ -9,32 +9,48 @@ color_red = (255, 0, 0)
 class Display(object):
 
     def __init__(self):
-        self.warning = False
-        self.shouldStop = False
-        self.pixels = neopixel.NeoPixel(board.D18, 1, brightness=0.5)
         self.thread = threading.Thread(target=self.loop)
+        self.flag = Event()
+        self.shouldStop = False
+
+        self.warning = False
+        self.pixels = neopixel.NeoPixel(board.D18, 1, brightness=0.5)
 
     def start(self):
+        self.flag.clear()
         self.thread.start()
 
     def loop(self):
         while (not self.shouldStop):
-            self.pixels.fill(color_green)
-            time.sleep(1)
-            self.pixels.fill(color_red)
-            time.sleep(1)
+            self.update()
+            self.flag.wait()
         self.pixels.deinit()
     
+    def update(self):
+        if (self.warning):
+            self.pixels.fill(color_red)
+        else:
+            self.pixels.fill(color_green)
+
     def stop(self):
         self.shouldStop = True
+        self.flag.set()
         self.thread.join()
+    
+    def setWarning(self, value):
+        self.warning = value
+        self.flag.set()
 
 
 d = Display()
 try:
     d.start()
+
+    warn = False
     while (True):
-        pass
+        d.setWarning(warn)
+        warn = not warn
+        time.sleep(1)
 except KeyboardInterrupt:
     pass
 finally:
