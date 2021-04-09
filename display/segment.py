b2c267a3dca0a690681675b76ed15b6193c9c67c
Krzysztof Antoniak <krzysztof@antoniak.in>
2021-04-09 22:09:39 +0200

    update

--- display/segment.py
+++ display/segment.py
@@ -1,10 +1,20 @@
 import RPi.GPIO as GPIO
+import time
 
-pin = 21
+# ABCDEFGABCDEFG...
+pins = [
+    1, 12, 5, 0, 11, 8, 7, # leftmost digit
+    20, 21, 26, 13, 6, 16, 19 # rightmost digit
+]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(pin, GPIO.OUT)
+GPIO.setup(pins, GPIO.OUT)
+GPIO.output(pins, GPIO.HIGH)
 
-GPIO.output(pin, GPIO.LOW)
+while (True):
+    for i in range(len(pins)):
+        GPIO.output(pins[i], GPIO.LOW)
+        time.sleep(0.2)
+        GPIO.output(pins[i], GPIO.HIGH)
