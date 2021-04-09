fad6708f59f291d8c3e2a0444655bac1c2b24cb3
Krzysztof Antoniak <krzysztof@antoniak.in>
2021-04-09 23:32:07 +0200

    update

--- display/segment.py
+++ display/segment.py
@@ -1,44 +1,74 @@
 import RPi.GPIO as GPIO
 import time
 
-# ABCDEFGABCDEFG...
+# [ABCDEFG, ABCDEFG]
 pinsInUse = [
-    1, 12, 5, 0, 11, 8, 7, # leftmost digit
-    20, 21, 26, 13, 6, 16, 19 # rightmost digit
+    [20, 21, 26, 13, 6, 16, 19] # rightmost digit
+    [1, 12, 5, 0, 11, 8, 7], # leftmost digit
 ]
 
-# Maps digit to list of segments, where 0=A, 1=B etc.
-digits = [
-    [0, 1, 2, 3, 4, 5],
-    [1, 2],
-    [0, 1, 3, 4, 6],
-    [0, 1, 2, 3, 6],
-    [1, 2, 5, 6],
-    [0, 2, 3, 5, 6],
-    [0, 2, 3, 4, 5, 6],
-    [0, 1, 2],
-    [0, 1, 2, 3, 4, 5, 6],
-    [0, 1, 2, 3, 5, 6]
-]
+class FJ8201BH(object):
+
+    # Maps digit to list of segments, where 0=A, 1=B etc.
+    DIGIT_TO_SEGMENTS = [
+        [0, 1, 2, 3, 4, 5],
+        [1, 2],
+        [0, 1, 3, 4, 6],
+        [0, 1, 2, 3, 6],
+        [1, 2, 5, 6],
+        [0, 2, 3, 5, 6],
+        [0, 2, 3, 4, 5, 6],
+        [0, 1, 2],
+        [0, 1, 2, 3, 4, 5, 6],
+        [0, 1, 2, 3, 5, 6]
+    ]
+
+    ALL_SEGMENTS = range(0, 6)
+
+    def __init__(self, digitsToPins):
+        self.digitsToPins = digitsToPins
+        self.display = [None, None] # Right to left
+
+    def start(self):
+        allPins = self._flatten(self.digitsToPins)
+        GPIO.setup(allPins, GPIO.OUT)
+        GPIO.output(allPins, GPIO.HIGH)
+
+    def stop(self):
+        allPins = self._flatten(self.digitsToPins)
+        GPIO.output(allPins, GPIO.HIGH)
+
+    def _setDigit(self, numFromRight, value):
+        if (self.display[numFromRight] == value):
+            return
+
+        if (value == None):
+            GPIO.output(ALL_SEGMENTS, GPIO.HIGH)
+            self.display[numFromRight] = None
+            return
+
+        segments = DIGIT_TO_SEGMENTS[value]
+        segmentsToPins = self.digitsToPins(numFromRight)
+        pins = list(map(lambda s: segmentsToPins[s], segments))
+        GPIO.output(pins, GPIO.LOW)
+        nonPins = ALL_SEGMENTS - pins
+        GPIO.output(nonPins, GPIO.HIGH)
+
+        self.display[numFromRight] = value
+
+    def _flatten(self, l):
+        return [item for sublist in l for item in sublist]
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(pinsInUse, GPIO.OUT)
-GPIO.output(pinsInUse, GPIO.HIGH)
+c = FJ8201BH(pinsInUse)
+toDisplay = [None] + range(0, 9)
 
-# lambda p: pins[p]
-def get_pin(p):
-    print("PIN: "+str(p)+" -> "+str(pinsInUse[p]))
-    return pinsInUse[p]
-
+c.start()
 while (True):
-    for i in range(len(digits)):
-        print(digits[i])
-        pins = list(map(get_pin, digits[i]))
-        print(pins)
-        GPIO.output(pins, GPIO.LOW)
-        time.sleep(0.2)
-        GPIO.output(pins, GPIO.HIGH)
+    for i in range(len(toDisplay)):
+        c._setDigit(1, toDisplay[i])
 
-GPIO.output(pinsInUse, GPIO.LOW)
+c.stop()
