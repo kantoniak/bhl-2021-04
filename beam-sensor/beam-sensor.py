c3c1b218e77e6998fd927eeb8b315fe1f22a1f92
avan1235 <macieekprocyk@gmail.com>
2021-04-09 23:05:25 +0200

    setup rpi gpio dependency

--- beam-sensor/beam-sensor.py
+++ beam-sensor/beam-sensor.py
@@ -2,11 +2,13 @@ import RPi.GPIO as GPIO
 
 BEAM_PIN = 17
 
+
 def break_beam_callback(channel):
     if GPIO.input(BEAM_PIN):
         print("beam unbroken")
     else:
         print("beam broken")
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
