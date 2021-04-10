cdff0263b318f98dc489366e4f4d9dbdda45f75c
Maciej Procyk <macieekprocyk@gmail.com>
2021-04-10 11:55:30 +0200

    core control center (#12)

--- display/demo.py
+++ display/demo.py
@@ -2,19 +2,11 @@ import board
 import RPi.GPIO as GPIO
 import time
 from display import Display
-from FJ8201BH import *
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-DISPLAY_PIN = board.D18
-LED_BRIGHTNESS = 1
-
-COUNTER_DIGITS = [] # [[ABCDEFG], [ABCDEFG]]
-COUNTER_DIGITS.append([20, 21, 26, 13, 6, 16, 19]) # rightmost digit
-COUNTER_DIGITS.append([1, 12, 5, 0, 11, 8, 7]) # leftmost digit
-
-d = Display(DISPLAY_PIN, LED_BRIGHTNESS, COUNTER_DIGITS)
+d = Display()
 
 # Count upwards, trigger warning at 20
 try:
